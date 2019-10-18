124d7f2586e93e6191b5cea56ec190e2cdc5f91b
Roy Adventus <radventus@ayannah.com>
2019-10-18 12:06:01 +0700

    revised test unit messaging - post body jarcoal/httpmock: OK;

--- partner/adsmedia.go
+++ partner/adsmedia.go
@@ -3,9 +3,13 @@ package partner
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/jarcoal/httpmock"
 )
 
 type (
@@ -16,6 +20,49 @@ type (
 const URLendpoint string = "http://sms241.xyz/sms/api_sms_masking_send_json.php"
 
 func Send(con Config) (body []byte, err error) {
+	//NOTE(RA): check run in test or normal execution
+	//var DefaultTransport = http.DefaultTransport
+	if flag.Lookup("test.v") == nil {
+		log.Println("normal run")
+
+	} else {
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+		log.Println("run under go test")
+
+		//DONE mockup responder
+		httpmock.RegisterResponder("POST", "http://sms241.xyz/sms/api_sms_masking_send_json.php",
+			func(req *http.Request) (*http.Response, error) {
+				//DONE: mockup with parameters and POST
+				PayloadSmsOTP := make(map[string]interface{})
+				if err := json.NewDecoder(req.Body).Decode(&PayloadSmsOTP); err != nil {
+					return httpmock.NewStringResponse(http.StatusInternalServerError, ""), nil
+				}
+				result := fmt.Sprintf(`{
+					"Config": {
+						"apikey": "7a8f16c956ae0c1e50461972d972d228",
+						"callbackurl": "",
+						"datapacket": [
+							{
+								"number": "%s",
+								"message": "%s",
+								"sendingdatetime": ""
+							}
+						]
+					},
+					"Response": "{\"sending_respon\":[{\"globalstatus\":10,\"globalstatustext\":\"Success\",\"datapacket\":[{\"packet\":{\"number\":\"6282297335657\",\"sendingid\":1287265,\"sendingstatus\":10,\"sendingstatustext\":\"success\",\"price\":320}}]}]}",
+					"message": "SMS Sent"
+				}`, PayloadSmsOTP["phone_number"], PayloadSmsOTP["message"])
+				fmt.Println(result)
+				resp, err := httpmock.NewJsonResponse(http.StatusOK, result)
+				if err != nil {
+					return httpmock.NewStringResponse(http.StatusInternalServerError, err.Error()), nil
+				}
+				return resp, nil
+			},
+		)
+	}
+
 	//set payload
 	payload, _ := json.Marshal(con)
 	//send request
--- tests/messaging_sms_otp_test.go
+++ tests/messaging_sms_otp_test.go
@@ -3,14 +3,12 @@ package tests
 import (
 	"flag"
 	"fmt"
-	"log"
 	"messaging/router"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gavv/httpexpect"
-	"github.com/jarcoal/httpmock"
 )
 
 func TestSMSSendOTP(t *testing.T) {
@@ -48,49 +46,17 @@ func TestSMSSendOTP(t *testing.T) {
 		"message":      "Kode OTP Anda adalah 997823",
 	}
 
-	//NOTE(RA): check run in test or normal execution
-	//var DefaultTransport = http.DefaultTransport
-	if flag.Lookup("test.v") == nil {
-		log.Println("normal run")
-
-	} else {
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
-		log.Println("run under go test")
-
-		//TODO mockup responder
-		httpmock.RegisterResponder("POST", "/client/send",
-			func(req *http.Request) (*http.Response, error) {
-				resp, err := httpmock.NewJsonResponse(200, `{
-					"Config": {
-						"apikey": "7a8f16c956ae0c1e50461972d972d228",
-						"callbackurl": "",
-						"datapacket": [
-							{
-								"message": "Kode OTP Anda adalah 99782377",
-								"number": "082297335657",
-								"sendingdatetime": ""
-							}
-						]
-					},
-					"Response": "{\"sending_respon\":[{\"globalstatus\":10,\"globalstatustext\":\"Success\",\"datapacket\":[{\"packet\":{\"number\":\"6282297335657\",\"sendingid\":1287265,\"sendingstatus\":10,\"sendingstatustext\":\"success\",\"price\":320}}]}]}",
-					"message": "SMS Sent"
-				}`)
-				if err != nil {
-					return httpmock.NewStringResponse(503, err.Error()), nil
-				}
-				return resp, nil
-			},
-		)
-	}
-
 	// expect valid response
 	auth.POST("/client/send").
 		WithJSON(payload).
 		Expect().
 		Status(http.StatusOK).
 		JSON().
-		String()
+		Object()
 
-	log.Println(payload)
+	auth.POST("/client/send").WithJSON(nil).
+		Expect().
+		Status(http.StatusUnprocessableEntity).
+		JSON().
+		Object()
 }
