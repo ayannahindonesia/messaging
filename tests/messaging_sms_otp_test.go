410baa33f44f06f11802d11dfd38a0493c8f08c5
Roy Adventus <radventus@ayannah.com>
2019-10-23 14:43:13 +0700

    update swagger.yaml

--- tests/messaging_sms_otp_test.go
+++ tests/messaging_sms_otp_test.go
@@ -59,3 +59,61 @@ func TestSMSSendOTP(t *testing.T) {
 		JSON().
 		Object()
 }
+
+func TestMessageOTPList(t *testing.T) {
+	RebuildData()
+
+	api := router.NewRouter()
+
+	server := httptest.NewServer(api)
+
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+clientBasicToken)
+	})
+
+	clientToken := getClientLoginToken(e, auth, "0")
+
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+clientToken)
+	})
+
+	// expect valid response
+	auth.GET("/client/message_otp").
+		Expect().
+		Status(http.StatusOK).
+		JSON().
+		Object()
+	//get by phone number
+	obj := auth.GET("/client/message_otp").
+		WithQuery("phone_number", "08").
+		Expect().
+		Status(http.StatusOK).
+		JSON().
+		Object()
+	obj.ContainsKey("data").NotEmpty()
+	//get status success
+	obj = auth.GET("/client/message_otp").
+		WithQuery("status", "success").
+		Expect().
+		Status(http.StatusOK).
+		JSON().
+		Object()
+	//assert.Equal(t, 1, obj.ContainsKey("data").Values().Length())
+	//assert.Equal(t, "success", )
+	obj.ContainsKey("status").Value("success")
+
+	//negative test, invalid token
+	auth2 := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer invalid")
+	})
+	auth2.GET("/client/message_otp").
+		WithQuery("phone_number", "08").
+		Expect().
+		Status(http.StatusUnauthorized).
+		JSON().
+		Object()
+}
