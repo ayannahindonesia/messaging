67512f9aea473c20627dcd764d11fb4781ffb815
Roy Adventus <radventus@ayannah.com>
2019-10-24 10:44:28 +0700

    init notif fcm backend

--- handlers/notification.go
+++ handlers/notification.go
@@ -23,53 +23,34 @@ func MessageNotificationSend(c echo.Context) error {
 	//get messaging model
 	messaging := models.Messaging{}
 
-	//TODO: OTP generate 6 digit value
-	payloadRules := govalidator.MapData{
-		"phone_number": []string{"required"}, //TODO:, "id_phonenumber"
-		"message":      []string{"required"},
-	}
-	validate := validateRequestPayload(c, payloadRules, &messaging)
-	if validate != nil {
-		return returnInvalidResponse(http.StatusUnprocessableEntity, validate, "validation error")
-	}
-	//build otp request to partner
-	number := messaging.PhoneNumber
-	message := messaging.Message
-	//build request data
-	conf := partner.PrepareRequestData(number, message)
-	//send to API partner
-	response, err := partner.Send(conf)
-	if err != nil {
-		return returnInvalidResponse(http.StatusInternalServerError, err, "")
-	}
-	//get response from parsing json API partner
-	status, err := partner.GetStatusResponse(response)
-	/*
-		if err != nil {
-			return returnInvalidResponse(http.StatusInternalServerError, err, "Internal Error : "+err.Error())
-		}
-	*/
-	//TODO: check return value from API Partner
-	messaging.Status = status
-	//error message
-	if err == nil {
-		messaging.ErrorMessage = ""
-	} else {
-		messaging.ErrorMessage = err.Error()
-	}
-	//TODO: dinamic partner setting
-	messaging.Partner = "adsmedia"
-	//raw response from API partner
-	messaging.RawResponse = string(response)
-	//clientID from jwt
-	messaging.ClientID = clientID
-	//DONE: storing models
-	err = messaging.Create()
-	if err != nil {
-		return returnInvalidResponse(http.StatusInternalServerError, err, "Gagal menyimpan pesan OTP")
-	}
-	log.Println(string(response))
-	return c.JSON(http.StatusOK, messaging)
+	// Obtain a messaging.Client from the App.
+ctx := context.Background()
+client, err := app.Messaging(ctx)
+if err != nil {
+        log.Fatalf("error getting Messaging client: %v\n", err)
+}
+
+// This registration token comes from the client FCM SDKs.
+registrationToken := "YOUR_REGISTRATION_TOKEN"
+
+// See documentation on defining a message payload.
+message := &messaging.Message{
+        Data: map[string]string{
+                "score": "850",
+                "time":  "2:45",
+        },
+        Token: registrationToken,
+}
+
+// Send a message to the device corresponding to the provided
+// registration token.
+response, err := client.Send(ctx, message)
+if err != nil {
+        log.Fatalln(err)
+}
+// Response is a message ID string.
+fmt.Println("Successfully sent message:", response)
+	return c.JSON(http.StatusOK, "OK"
 }
 
 func MessageNotificationList(c echo.Context) error {
